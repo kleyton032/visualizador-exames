74f040c07dd9dbb688af197c7897995480ce14a9
Kleyton João <kleyton.joao@gmail.com>
2026-01-10 14:15:24 -0300

    implementation anexo module

--- ver-api/src/modules/anexos/anexo.controller.ts
+++ ver-api/src/modules/anexos/anexo.controller.ts
@@ -3,8 +3,30 @@ import { AnexoService } from './anexo.service';
 
 export class AnexoController {
   private service = new AnexoService();
-  create = async (req: Request, res: Response) => {
-    await this.service.create(req.body.documentoId, req.body.nomeArquivo);
+
+  listExames = async (req: Request, res: Response) => {
+    const exames = await this.service.listExames();
+    res.json(exames);
+  };
+
+
+  upload = async (req: Request, res: Response) => {
+    if (!req.file) {
+      return res.status(400).json({ error: 'No file uploaded' });
+    }
+
+    const { cd_paciente, cd_atendimento, id_exame, data, olho, observacoes, status } = req.body;
+
+    await this.service.upload(req.file, {
+      cd_paciente: Number(cd_paciente),
+      cd_atendimento: Number(cd_atendimento),
+      id_exame: Number(id_exame),
+      data: data,
+      olho: olho,
+      observacoes: observacoes,
+      status: status
+    });
+
     res.status(201).send();
   };
 }
--- ver-api/src/modules/anexos/anexo.service.ts
+++ ver-api/src/modules/anexos/anexo.service.ts
@@ -1,8 +1,43 @@
 import { AnexoRepository } from './anexo.repository';
+import fs from 'fs-extra';
+import path from 'path';
 
 export class AnexoService {
   private repo = new AnexoRepository();
-  async create(documentoId: number, nomeArquivo: string) {
-    await this.repo.create(documentoId, nomeArquivo);
+  async upload(file: Express.Multer.File, data: {
+    cd_paciente: number;
+    cd_atendimento: number;
+    id_exame: number;
+    data: string;
+    olho: string;
+    observacoes: string;
+    status: string;
+  }) {
+    const baseDir = '\\\\192.168.4.18\\C$\\anexos_exames';
+    const targetDir = path.join(baseDir, data.cd_paciente.toString());
+    const filename = `${data.cd_paciente}-${data.cd_atendimento}-${data.id_exame}-${data.data}.pdf`;
+    const targetPath = path.join(targetDir, filename);
+
+
+    await fs.ensureDir(targetDir);
+
+
+    await fs.move(file.path, targetPath, { overwrite: true });
+
+
+    await this.repo.saveAnexoExame({
+      cd_paciente: data.cd_paciente,
+      cd_atendimento: data.cd_atendimento,
+      id_exame: data.id_exame,
+      observacoes: data.observacoes,
+      olho: data.olho,
+      data: data.data,
+      caminho_anexo: targetPath,
+      statusdoc: data.status
+    });
+  }
+
+  async listExames() {
+    return this.repo.listExames();
   }
 }
